3d6f28481b9e6166ba53211a423dd47601b6f0b0
yyxi <178086254+yyxi@users.noreply.github.com>
2025-05-23 20:19:57 +0000

    fix: vite plugin cleanup

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,119 +1,60 @@
+/* eslint-disable typescript/no-unnecessary-boolean-literal-compare */
 /* eslint-disable typescript/no-non-null-assertion */
 import { Lang, parse as parseAST } from '@ast-grep/napi'
+import type { Api as VuePluginApi } from '@vitejs/plugin-vue'
 import { parseVueRequest } from '@vitejs/plugin-vue'
-import { type SFCStyleBlock, parse } from '@vue/compiler-sfc'
 import { REGEX } from 'cassiopeia'
 import MagicString from 'magic-string'
 import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import type { Plugin, ResolvedConfig } from 'vite'
+import { shallowRef, type ShallowRef } from 'vue'
+import { parse as _parseSFC, type SFCStyleBlock } from 'vue/compiler-sfc'
+import { ruleSetupClientExportSFC, ruleSetupClientImportSFCHelper, ruleSetupSSR } from './rules'
 
-const ruleSetupClientImportSFCHelper = {
-  rule: {
-    any: [
-      {
-        pattern: "import _export_sfc from '$$$'",
-      },
-      {
-        pattern: 'import _export_sfc from "$$$"',
-      },
-    ],
-  },
-}
+const createProperties = (config: ResolvedConfig) => {
+  const vueOptions = (
+    config.plugins.find((value) => value.name === 'vite:vue') as Plugin<VuePluginApi>
+  ).api?.options
 
-const ruleSetupClientExportSFC = {
-  rule: {
-    any: [
-      {
-        pattern: 'export default _sfc_main',
-      },
-    ],
-  },
-}
-
-const ruleSetupSSR = {
-  rule: {
-    any: [
-      {
-        any: [
-          {
-            pattern: {
-              context: '_sfc_main.setup = ($$$) => {$$$BODY}',
-              selector: 'statement_block',
-            },
-          },
-        ],
-        inside: {
-          inside: {
-            inside: {
-              regex: '_sfc_main\\.setup',
-            },
-            kind: 'assignment_expression',
-          },
-          kind: 'arrow_function',
-        },
-      },
-    ],
-  },
-}
-
-declare module '@vitejs/plugin-vue' {
-  interface VueQuery {
-    setup?: string
-  }
-}
-
-interface State {
-  devToolsEnabled: boolean
-  isDevelopment: boolean
-  sourceMap: boolean
-}
-
-interface StateProduction extends State {
-  variables: Map<string, Set<string>>
-}
-
-const configResolved = (config: ResolvedConfig, state: State) => {
   // https://github.com/vitejs/vite-plugin-vue/blob/main/packages/plugin-vue/src/index.ts#L146
-  state.isDevelopment = config.mode === 'development'
-  state.sourceMap =
-    // eslint-disable-next-line typescript/strict-boolean-expressions
-    config.command === 'build' ? !!config.build.sourcemap : true
-  state.devToolsEnabled =
-    // eslint-disable-next-line typescript/strict-boolean-expressions
-    !!config.define!.__VUE_PROD_DEVTOOLS__ || state.isDevelopment
-}
+  const parseSFC = vueOptions?.compiler?.parse ?? _parseSFC
+  const sourceMap = config.command === 'build' ? config.build.sourcemap !== false : true
 
-const initialState: State = {
-  devToolsEnabled: false,
-  isDevelopment: false,
-  sourceMap: false,
-}
+  // const developmentToolsEnabled =
+  //   vueOptions?.features?.prodDevtools === true ||
+  //   config?.define?.__VUE_PROD_DEVTOOLS__ === 'true' ||
+  //   !config.isProduction
 
-const createProductionPlugin = (): Plugin[] => {
-  const state: StateProduction = {
-    ...initialState,
-    variables: new Map<string, Set<string>>(),
+  return {
+    // developmentToolsEnabled,
+    parseSFC,
+    sourceMap,
   }
+}
+
+type Properties = ReturnType<typeof createProperties>
+
+const createProductionPlugins = (properties: ShallowRef<Properties | undefined>): Plugin[] => {
+  const indice = new Map<string, Set<string>>()
 
   return [
     {
-      buildStart() {
-        state.variables.clear()
-      },
-      configResolved: (config) => configResolved(config, state),
       enforce: 'pre',
-      name: '@cassiopeia/vite/production-pre',
+      name: '@cassiopeia/vite:pre-production',
+
+      apply: (_, { command, isPreview }) => (command === 'serve' ? isPreview === true : true),
+
+      buildStart() {
+        indice.clear()
+      },
+
       transform: {
         async handler(source, id) {
-          if (state.isDevelopment) {
-            return
-          }
-
           const updateStateVariables = (styles: string) => {
-            const set = state.variables.has(filename)
-              ? state.variables.get(filename)!
-              : (state.variables.set(filename, new Set()), state.variables.get(filename)!)
+            const set = indice.has(filename)
+              ? indice.get(filename)!
+              : (indice.set(filename, new Set()), indice.get(filename)!)
 
             for (const match of styles.matchAll(REGEX)) {
               set.add(['--', ...match.slice(1, 3)].join('-'))
@@ -128,7 +69,7 @@ const createProductionPlugin = (): Plugin[] => {
           const { filename, query } = parseVueRequest(id)
 
           if (filename.endsWith('.vue') && query.vue !== true) {
-            const parseResult = parse(source)
+            const parseResult = _parseSFC(source)
 
             for (const style of parseResult.descriptor.styles) {
               const content = await getStyleContent(style)
@@ -142,20 +83,19 @@ const createProductionPlugin = (): Plugin[] => {
     },
     {
       enforce: 'post',
-      name: '@cassiopeia/vite/production-post',
+      name: '@cassiopeia/vite:post-production',
+
+      apply: (_, { command, isPreview }) => (command === 'serve' ? isPreview === true : true),
+
       transform: {
         handler(source, id, options) {
-          if (state.isDevelopment) {
-            return
-          }
-
           const isSSR = options?.ssr === true
 
           const { filename, query } = parseVueRequest(id)
 
           if (
-            state.variables.has(filename) &&
-            state.variables.get(filename)?.size !== 0 &&
+            indice.has(filename) &&
+            indice.get(filename)?.size !== 0 &&
             Object.values(query).filter((value) => value !== undefined).length === 0
           ) {
             if (isSSR) {
@@ -168,7 +108,7 @@ const createProductionPlugin = (): Plugin[] => {
               } else {
                 const magic = new MagicString(source)
 
-                const variables = Array.from(state.variables.get(filename)!)
+                const variables = Array.from(indice.get(filename)!)
                   .map((value) => `"${value}"`)
                   .join(', ')
 
@@ -189,7 +129,7 @@ const createProductionPlugin = (): Plugin[] => {
 
                 // this.info(`Update successful, ${JSON.stringify(query)}`)
 
-                return state.sourceMap
+                return properties.value!.sourceMap === true
                   ? {
                       code: magic.toString(),
                       map: magic.generateMap(),
@@ -209,7 +149,7 @@ const createProductionPlugin = (): Plugin[] => {
               } else {
                 const magic = new MagicString(source)
 
-                const variables = Array.from(state.variables.get(filename)!)
+                const addVariables = Array.from(indice.get(filename)!)
                   .map((value) => `"${value}"`)
                   .join(', ')
 
@@ -218,7 +158,7 @@ const createProductionPlugin = (): Plugin[] => {
                   `const _sfc_setup_cassiopeia = _sfc_main.setup;`,
                   `_sfc_main.setup = (props, ctx) => {`,
                   `const __cassiopeia = __useCassiopeia();`,
-                  `__cassiopeia.add([${variables}]);`,
+                  `__cassiopeia.add([${addVariables}]);`,
                   `__cassiopeia.update(false);`,
                   `return _sfc_setup_cassiopeia ? _sfc_setup_cassiopeia(props, ctx) : void 0;`,
                   `};`,
@@ -237,7 +177,7 @@ const createProductionPlugin = (): Plugin[] => {
 
                 // this.info(`Update successful, ${JSON.stringify(query)}`)
 
-                return state.sourceMap
+                return properties.value!.sourceMap === true
                   ? {
                       code: magic.toString(),
                       map: magic.generateMap(),
@@ -255,16 +195,16 @@ const createProductionPlugin = (): Plugin[] => {
   ]
 }
 
-const createDevelopmentPlugin = (): Plugin => {
-  const state: State = { ...initialState }
-
-  return {
-    configResolved: (config) => configResolved(config, state),
+const createDevelopmentPlugins = (properties: ShallowRef<Properties | undefined>): Plugin[] => [
+  {
     enforce: 'post',
-    name: '@cassiopeia/vite/development',
+    name: '@cassiopeia/vite:development',
+
+    apply: (_, { command, isPreview }) => command === 'serve' && isPreview === false,
+
     transform: {
       handler(source, id, options) {
-        if (!state.isDevelopment || !state.devToolsEnabled || options?.ssr === true) {
+        if (options?.ssr === true) {
           return
         }
 
@@ -281,7 +221,7 @@ const createDevelopmentPlugin = (): Plugin => {
             `\n__cassiopeiaUpdateStyle(__vite__id, __vite__css, import.meta.hot.dispose.bind(import.meta.hot))`,
           )
 
-          return state.sourceMap
+          return properties.value!.sourceMap
             ? {
                 code: magic.toString(),
                 map: magic.generateMap(),
@@ -292,10 +232,21 @@ const createDevelopmentPlugin = (): Plugin => {
         return
       },
     },
-  }
-}
-
-export const cassiopeia: () => Plugin[] = () => [
-  createDevelopmentPlugin(),
-  ...createProductionPlugin(),
+  },
 ]
+
+export const cassiopeia: () => Plugin[] = () => {
+  const properties = shallowRef<Properties>()
+
+  return [
+    {
+      name: '@cassiopeia/vite:configResolved',
+
+      configResolved(value) {
+        properties.value = createProperties(value)
+      },
+    },
+    ...createDevelopmentPlugins(properties),
+    ...createProductionPlugins(properties),
+  ]
+}
